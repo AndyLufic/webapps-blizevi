091ce04d92c5c3aeb22637957b51f31c2790e1d6
AndyLufic <andreaslucic9@gmail.com>
2022-10-11 08:23:30 +0200

    rjesen zadatak 1

---
+++ zadatak1-blitz.js
@@ -0,0 +1,18 @@
+a=[{1: "a"},{1: "c", 2: "A"}, {2:"c"}]
+
+function kljuc(){
+    b= []
+    for (let i of a ){
+        if(a['key']==null){
+           
+        }
+        else{
+            b.push(a[i])
+
+         }
+
+    console.log(b)
+}
+}
+
+kljuc()
